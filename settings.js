69c2043ab13da4471865c5d59a598f36fb992358
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2023-02-24 14:46:51 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -41,24 +41,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "ikybot-Mdོ" //namabot kalian
+global.ownername = "ᴹᴿ᭄ rizki ×፝֟͜×" //nama kalian
+global.myweb = "https://www.youtube.com/@RIZKIIRFAN" //bebas asal jan hapus
+global.youtube = "https://www.youtube.com/@RIZKIIRFAN" //bebas asal jan hapus
+global.myweb2 = "https://www.youtube.com/@RIZKIIRFAN" //bebas
+global.email = "@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone list : https://www.php.net/manual/en/timezones.php 
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6285878836361'] //premium user
+global.owner = ["6285878836361", "6285878836361"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285878836361" // nomor wa kalian
+global.ownernomerr = "+6285878836361" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© ikybot-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh ikybot6285878836361' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 //type menu for menu
 global.typemenu = 'document' //
